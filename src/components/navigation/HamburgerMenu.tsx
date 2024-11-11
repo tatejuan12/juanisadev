20c3c2ea6661997fd4cc4fcb5bd5651b4f9f711b
tatejuan12 <tatejuan11@gmail.com>
2024-11-12 10:29:00 +1100

    Removed placeholder nav bar items.

--- src/components/navigation/HamburgerMenu.tsx
+++ src/components/navigation/HamburgerMenu.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { useState, useEffect, useRef } from "react";
-import { useSpring, animated } from "react-spring";
+import {useState, useEffect, useRef} from "react";
+import {useSpring, animated} from "react-spring";
 import Hamburger from "hamburger-react";
 import styles from "@styles/modules/_navbar.module.scss";
-import { NavItemPage } from "@components/navigation/NavItemPage";
-import { NavItemSvg } from "@components/navigation/NavItemSvg";
-import { LinkedinSVG } from "@components/navigation/svgs/linkedinSVG";
-import { GithubSVG } from "@components/navigation/svgs/githubSVG";
-import { EmailSVG } from "@components/navigation/svgs/emailSVG";
+import {NavItemPage} from "@components/navigation/NavItemPage";
+import {NavItemSvg} from "@components/navigation/NavItemSvg";
+import {LinkedinSVG} from "@components/navigation/svgs/linkedinSVG";
+import {GithubSVG} from "@components/navigation/svgs/githubSVG";
+import {EmailSVG} from "@components/navigation/svgs/emailSVG";
 
 export const HamburgerMenu = () => {
     const [isOpen, setOpen] = useState(false);
@@ -34,40 +34,27 @@ export const HamburgerMenu = () => {
     const menuAnimation = useSpring({
         maxHeight: isOpen ? 200 : 0,
         opacity: isOpen ? 1 : 0,
-        config: { tension: 200, friction: 20 },
+        config: {tension: 200, friction: 20},
     });
 
     return (
         <div className={styles.nav_collapsible} ref={menuRef}>
-            <Hamburger toggled={isOpen} toggle={setOpen} />
+            <Hamburger toggled={isOpen} toggle={setOpen}/>
             {/* Collapsible Nav Items (Modify as needed) */}
             <animated.div className={styles.nav_collapsible_menu} style={menuAnimation}>
-                <NavItemPage
-                    className={styles.nav_rightbar_pagelinks_pagelink}
-                    href="about"
-                />
-                <NavItemPage
-                    className={styles.nav_rightbar_pagelinks_pagelink}
-                    href="about"
-                />
-                <NavItemPage
-                    className={styles.nav_rightbar_pagelinks_pagelink}
-                    href="about"
-                />
-
                 <div className={styles.nav_collapsible_menu_icons}>
                     <NavItemSvg
-                        svg={<LinkedinSVG />}
+                        svg={<LinkedinSVG/>}
                         className={styles.nav_rightbar_social}
                         href="https://www.linkedin.com/in/juan-tate/"
                     />
                     <NavItemSvg
-                        svg={<GithubSVG />}
+                        svg={<GithubSVG/>}
                         className={styles.nav_rightbar_social}
                         href="https://github.com/tatejuan12"
                     />
                     <NavItemSvg
-                        svg={<EmailSVG />}
+                        svg={<EmailSVG/>}
                         className={styles.nav_rightbar_social}
                         href="mailto: tatejuan11@gmail.com"
                     />
--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,50 +1,51 @@
 import styles from "@styles/modules/_navbar.module.scss";
 import ThemeToggleButton from "@components/themeButton/themeToggleButton";
-import { useState } from "react";
-import { LogoSVG } from "./svgs/logoSVG";
-import { LinkedinSVG } from "./svgs/linkedinSVG";
-import { GithubSVG } from "./svgs/githubSVG";
-import { EmailSVG } from "./svgs/emailSVG";
-import { NavItemSvg } from "./NavItemSvg";
-import { NavItemPage } from "./NavItemPage";
-import { HamburgerMenu } from "./HamburgerMenu";
+import {useState} from "react";
+import {LogoSVG} from "./svgs/logoSVG";
+import {LinkedinSVG} from "./svgs/linkedinSVG";
+import {GithubSVG} from "./svgs/githubSVG";
+import {EmailSVG} from "./svgs/emailSVG";
+import {NavItemSvg} from "./NavItemSvg";
+import {NavItemPage} from "./NavItemPage";
+import {HamburgerMenu} from "./HamburgerMenu";
+
 const NavBar = () => {
-  return (
-    <nav className={styles.nav}>
-      <div className={styles.nav_logo}>
-        <LogoSVG className={styles.nav_logo_image} />
-      </div>
-      <div className={styles.nav_rightbar}>
-        <div className={styles.nav_rightbar_pagelinks}>
-          <NavItemPage
-            className={styles.nav_rightbar_pagelinks_pagelink}
-            href="about"
-          ></NavItemPage>
-        </div>
-        <NavItemSvg
-          svg={<LinkedinSVG />}
-          className={styles.nav_rightbar_social}
-          href="https://www.linkedin.com/in/juan-tate/"
-        />
-        <NavItemSvg
-          svg={<GithubSVG />}
-          className={styles.nav_rightbar_social}
-          href="https://github.com/tatejuan12"
-        />
-        <NavItemSvg
-          svg={<EmailSVG />}
-          className={styles.nav_rightbar_social}
-          href="mailto: tatejuan11@gmail.com"
-        />
-        <div className={styles.nav_rightbar_themetoggle}>
-          <ThemeToggleButton
-            className={styles.nav_rightbar_themetoggle_theme_toggle_button}
-          />
-        </div>
-        <HamburgerMenu />
-      </div>
-    </nav>
-  );
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.nav_logo}>
+                <LogoSVG className={styles.nav_logo_image}/>
+            </div>
+            <div className={styles.nav_rightbar}>
+                {/*<div className={styles.nav_rightbar_pagelinks}>*/}
+                {/*  <NavItemPage*/}
+                {/*    className={styles.nav_rightbar_pagelinks_pagelink}*/}
+                {/*    href="about"*/}
+                {/*  ></NavItemPage>*/}
+                {/*</div>*/}
+                <NavItemSvg
+                    svg={<LinkedinSVG/>}
+                    className={styles.nav_rightbar_social}
+                    href="https://www.linkedin.com/in/juan-tate/"
+                />
+                <NavItemSvg
+                    svg={<GithubSVG/>}
+                    className={styles.nav_rightbar_social}
+                    href="https://github.com/tatejuan12"
+                />
+                <NavItemSvg
+                    svg={<EmailSVG/>}
+                    className={styles.nav_rightbar_social}
+                    href="mailto: tatejuan11@gmail.com"
+                />
+                <div className={styles.nav_rightbar_themetoggle}>
+                    <ThemeToggleButton
+                        className={styles.nav_rightbar_themetoggle_theme_toggle_button}
+                    />
+                </div>
+                <HamburgerMenu/>
+            </div>
+        </nav>
+    );
 };
 
 export default NavBar;
